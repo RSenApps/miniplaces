58c3c5c49f46213368afe92b8eafdd5566b54668
Ryan Senanayake <rsenapps@gmail.com>
2017-10-28 13:16:02 -0400

    x

--- model/tensorflow/vgg16_train.py
+++ model/tensorflow/vgg16_train.py
@@ -21,6 +21,25 @@ path_save = 'alexnet'
 start_from = ''
 
 
+# Construct dataloader
+opt_data_train = {
+    'data_h5': 'miniplaces_256_train.h5',
+    'data_root': '../../../images/',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../../data/train.txt', # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': True
+    }
+opt_data_val = {
+    'data_h5': 'miniplaces_256_val.h5',
+    'data_root': '../../../images/',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../../data/val.txt',   # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': False
+    }
 
 #loader_train = DataLoaderDisk(**opt_data_train)
 #loader_val = DataLoaderDisk(**opt_data_val)
