058848dff7ac0c3604a6998433f26cfe072481cf
Ryan Senanayake <rsenapps@gmail.com>
2017-10-28 13:21:31 -0400

    reduce batch

--- model/tensorflow/alexnet_train.py
+++ model/tensorflow/alexnet_train.py
@@ -4,14 +4,14 @@ import tensorflow as tf
 from DataLoader import *
 
 # Dataset Parameters
-batch_size = 128
+batch_size = 200
 load_size = 256
 fine_size = 224
 c = 3
 data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 
 # Training Parameters
-learning_rate = 0.01
+learning_rate = 0.001
 dropout = 0.5 # Dropout, probability to keep units
 training_iters = 100000
 step_display = 50
--- model/tensorflow/vgg16_train.py
+++ model/tensorflow/vgg16_train.py
@@ -3,21 +3,20 @@ import numpy as np
 import tensorflow as tf
 from DataLoader import *
 import vgg16
-
 # Dataset Parameters
-batch_size = 200
+batch_size = 128
 load_size = 256
 fine_size = 224
 c = 3
 data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 
 # Training Parameters
-learning_rate = 0.001
+learning_rate = 0.01
 dropout = 0.5 # Dropout, probability to keep units
 training_iters = 100000
 step_display = 50
 step_save = 10000
-path_save = 'alexnet'
+path_save = 'vgg16'
 start_from = ''
 
 
