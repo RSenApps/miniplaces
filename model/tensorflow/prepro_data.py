3aaca7d0391e81451a3d1dea532d6ee9cc8da997
Ryan Senanayake <rsenapps@gmail.com>
2017-11-02 20:13:31 -0400

    data augmentation

--- model/tensorflow/prepro_data.py
+++ model/tensorflow/prepro_data.py
@@ -57,7 +57,7 @@ def createH5(params):
 
 		crop = np.random.randint(70, 120, 1)
 		startx = image.shape[1]//2-(crop//2)
-    		starty = image.shape[0]//2-(crop//2)    
+    	starty = image.shape[0]//2-(crop//2)    
 		image = image[starty:starty+crop,startx:startx+crop]
     
 		image = scipy.misc.imresize(image, (params['img_resize'],params['img_resize']))
