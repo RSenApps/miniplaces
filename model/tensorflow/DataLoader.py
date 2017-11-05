48438be0d4a1ff369d63126ed79c5afccf220637
Ryan Senanayake <rsenapps@gmail.com>
2017-11-05 00:05:51 -0700

    data laoder

--- model/tensorflow/DataLoader.py
+++ model/tensorflow/DataLoader.py
@@ -63,7 +63,7 @@ class DataLoaderH5(object):
                 startx = np.random.randint(0, image.shape[1]-(crop))
                 starty = np.random.randint(0, image.shape[0]-(crop))
                 image = image[starty:starty+crop,startx:startx+crop, :]
-                images_batch[i, ...] = scipy.ndimage.zoom(image, (zoom,zoom, 1.0))
+                images_batch[i, ...] = image.repeat(zoom, 0).repeat(zoom, 1)
 
                 #offset_h = np.random.random_integers(0, self.load_size-self.fine_size)
                 #offset_w = np.random.random_integers(0, self.load_size-self.fine_size)
