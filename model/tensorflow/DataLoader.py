f6c84e0adbe37ba4b70f0504a161d286c144191f
Ryan Senanayake <rsenapps@gmail.com>
2017-11-04 22:18:12 -0700

    data laoder

--- model/tensorflow/DataLoader.py
+++ model/tensorflow/DataLoader.py
@@ -11,15 +11,13 @@ class DataLoaderH5(object):
         self.fine_size = int(kwargs['fine_size'])
         self.data_mean = np.array(kwargs['data_mean'])
         self.randomize = kwargs['randomize']
-        self.current_data_batch = 0
-        self.batch_count = 4
 
         # read data info from lists
         self.f = h5py.File(kwargs['data_h5'], "r")
-        self.im_set = np.array(self.f['images'][0 : len(self.f['images']) / self.batch_count])
-        self.lab_set = np.array(self.f['labels'][0 : len(self.f['images']) / self.batch_count])
+        self.im_set = np.array(self.f['images'])
+        self.lab_set = np.array(self.f['labels'])
 
-        self.num = self.im_set.shape[0] / self.batch_count
+        self.num = self.im_set.shape[0]
         assert self.im_set.shape[0]==self.lab_set.shape[0], '#images and #labels do not match!'
         assert self.im_set.shape[1]==self.load_size, 'Image size error!'
         assert self.im_set.shape[2]==self.load_size, 'Image size error!'
@@ -38,16 +36,34 @@ class DataLoaderH5(object):
             image = self.im_set[self._idx]
             image = image.astype(np.float32)/255. - self.data_mean
             if self.randomize:
-                flip = np.random.random_integers(0, 1)
-                if flip>0:
-                    image = image[:,::-1,:]
-                offset_h = np.random.random_integers(0, self.load_size-self.fine_size)
-                offset_w = np.random.random_integers(0, self.load_size-self.fine_size)
+                bg_value = np.median(image)
+                angle = np.random.randint(-15,15,1)
+                image = scipy.misc.imrotate(image,angle)
+                shiftx = np.random.randint(-10, 10, 1)
+                shifty = np.random.randint(-10, 10, 1)
+                image = scipy.ndimage.shift(image,[shiftx, shifty, 0], cval=bg_value)
+            
+                s_vs_p = 0.5
+                amount = 0.001
+                out = np.copy(image)
+                # Salt mode
+                num_salt = np.ceil(amount * image.size * s_vs_p)
+                coords = [np.random.randint(0, j - 1, int(num_salt)) for j in image.shape]
+                image[coords] = 1
+            
+                if (np.random.randint(0, 1, 1)):
+                    image = np.flip(image)
+
+                crop = np.random.randint(70, 120, 1)[0]
+                startx = image.shape[1]/2-(crop/2)
+                starty = image.shape[0]/2-(crop/2)
+                image = image[starty:starty+crop,startx:startx+crop, :]
+                images_batch[i, ...] = scipy.misc.imresize(image, (self.fine_size,self.fine_size))
             else:
                 offset_h = (self.load_size-self.fine_size)//2
                 offset_w = (self.load_size-self.fine_size)//2
-
-            images_batch[i, ...] = image[offset_h:offset_h+self.fine_size, offset_w:offset_w+self.fine_size, :]
+                images_batch[i, ...] = image[offset_h:offset_h+self.fine_size, offset_w:offset_w+self.fine_size, :]
+            
             labels_batch[i, ...] = self.lab_set[self._idx]
             
             self._idx += 1
