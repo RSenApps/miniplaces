e9cb19cbc6489cd1223a1e0490b3b10a3cb8798f
Ryan Senanayake <rsenapps@gmail.com>
2017-11-18 16:34:31 -0500

    exponential decay of lr

--- model/tensorflow/resnet_train.py
+++ model/tensorflow/resnet_train.py
@@ -16,7 +16,7 @@ data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 learning_rate = 0.05
 learning_rate_decay = .8
 dropout = 0.5 # Dropout, probability to keep units
-training_iters = 5000
+training_iters = 1000
 
 step_display = 100
 step_save = 1000
